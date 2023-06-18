706dd19250c015adff1c8f1f2199c021a1825b0f
SavaBM <128490103+SavaBM@users.noreply.github.com>
2023-06-18 14:49:07 +0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,15 +6,14 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_adding_success(self):
-        assert self.calc.adding(self, 1, 1) == 2
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 54, 24) == 30
 
-    def test_adding_unsucces(self):
-        assert self.calc.adding(self, 1, 1) == 3
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 7) == 14
 
-    def test_zero_division(self):
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1, 0)
+    def test_division_succes(self):
+        assert  self.calc.division(self, 1000, 100) == 10
 
     def teardown(self):
         print('Выполнение метода Teardown')
